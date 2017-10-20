e23b91598efc4586026d295c74b7e0695854ec18
Martin Page <wookoouk@gmail.com>
2017-10-20 13:09:43 +0100

    tidy up

--- controllers/index.js
+++ controllers/index.js
@@ -18,72 +18,38 @@ function compareScans(current, previous) {
         return current;
     }
 
-    current.readsFolders = current.readsFolders.map(rf => {
-        rf.sizePrevious = 0;
-        rf.sizeHumanPrevious = 0;
-        rf.sizeDifference = 0;
-        rf.sizeDifferenceHuman = '0';
-        const pFound = previous.readsFolders.filter(prf => {
-            return prf.name === rf.name;
-        });
+    function buildComparison(currentFolders, previousFolders) {
 
-        if (pFound && pFound.length) {
-            rf.sizePrevious = pFound[0].size;
-            rf.sizeHumanPrevious = pFound[0].sizeHuman;
-            rf.sizeDifference = rf.size - pFound[0].size;
-            if (Math.abs(rf.sizeDifference) > 0) {
-                rf.sizeDifferenceHuman = `${rf.sizeDifference > 0 ? '+' : '-'}  ${filesize(Math.abs(rf.sizeDifference))}`
+        return currentFolders.map(cf => {
+
+            cf.sizePrevious = 0;
+            cf.sizeHumanPrevious = 0;
+            cf.sizeDifference = 0;
+            cf.sizeDifferenceHuman = '0';
+
+
+            const pFound = previousFolders.filter(pf => {
+                return pf.name === cf.name;
+            });
+
+            if (pFound && pFound.length) {
+                cf.sizePrevious = pFound[0].size;
+                cf.sizeHumanPrevious = pFound[0].sizeHuman;
+                cf.sizeDifference = cf.size - pFound[0].size;
+                if (Math.abs(cf.sizeDifference) !== 0) {
+                    cf.sizeDifferenceHuman = `${cf.sizeDifference > 0 ? '+' : '-'}  ${filesize(Math.abs(cf.sizeDifference))}`
+                }
             }
-        }
 
-        return rf;
-    });
+            return cf;
 
-    current.scratchFolders = current.scratchFolders.map(sf => {
-        sf.sizePrevious = 0;
-        sf.sizeHumanPrevious = 0;
-        sf.sizeDifference = 0;
-        sf.sizeDifferenceHuman = '0';
-        const pFound = previous.readsFolders.filter(prf => {
-            return prf.name === sf.name;
-        });
+        })
 
-        if (pFound && pFound.length) {
-            sf.sizePrevious = pFound[0].size;
-            sf.sizeHumanPrevious = pFound[0].sizeHuman;
-            sf.sizeDifference = sf.size - pFound[0].size;
-            if (Math.abs(sf.sizeDifference) > 0) {
-                sf.sizeDifferenceHuman = `${sf.sizeDifference > 0 ? '+' : '-'}  ${filesize(Math.abs(sf.sizeDifference))}`
-            }
-        }
+    }
 
-        return sf;
-    });
-
-    current.homeFolders = current.homeFolders.map(hf => {
-        hf.sizePrevious = 0;
-        hf.sizeHumanPrevious = 0;
-        hf.sizeDifference = 0;
-        hf.sizeDifferenceHuman = '0';
-        const pFound = previous.scratchFolders.filter(phf => {
-            return phf.name === hf.name;
-        });
-
-        if (pFound && pFound.length) {
-            hf.sizePrevious = pFound[0].size;
-
-            console.log(hf.name, 'previous', pFound[0].size, 'current', hf.size, 'difference', hf.size - pFound[0].size);
-
-            hf.sizeHumanPrevious = pFound[0].sizeHuman;
-            hf.sizeDifference = hf.size - pFound[0].size;
-
-            if (hf.sizeDifference !== 0) {
-                hf.sizeDifferenceHuman = `${hf.sizeDifference > 0 ? '+' : '-'}  ${filesize(Math.abs(hf.sizeDifference))}`;
-            }
-        }
-
-        return hf;
-    });
+    current.readsFolders = buildComparison(current.readsFolders, previous.readsFolders);
+    current.scratchFolders = buildComparison(current.scratchFolders, previous.scratchFolders);
+    current.homeFolders = buildComparison(current.homeFolders, previous.homeFolders);
 
     return current;
 
