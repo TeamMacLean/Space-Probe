728276f0cb523ec6dab132ad54c43f729ea2b695
Martin Page <wookoouk@gmail.com>
2017-10-30 15:12:35 +0000

    remove presave

--- models/scan.js
+++ models/scan.js
@@ -5,18 +5,13 @@ const moment = require('moment');
 
 const Scan = thinky.createModel("Scan", {
     id: type.string(),
-    date: type.date().default(new Date()),
-    dateHuman: type.string().required()
-});
-
-Scan.pre('save', function () {
-    this.dateHuman = moment(this.date).format('MMMM Do YYYY, HH:mm');
+    date: type.date().default(new Date())
 });
 
 module.exports = Scan;
 
 Scan.define('getDateHuman', function () {
-    return moment(this.date).format('DD/MM/YY, HH:mm');
+    return moment(this.date).format('DD/MM/YY HH:mm');
 });
 
 const Location = require('./location');
